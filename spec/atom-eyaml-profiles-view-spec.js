3cab99239a3945319635dce1464dc9e1a5db3377
af6140 <daweiwang.gatekeeper@gmail.com>
2017-10-14 09:19:31 -0400

    initial

---
+++ spec/atom-eyaml-profiles-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomEyamlProfilesView from '../lib/atom-eyaml-profiles-view';
+
+describe('AtomEyamlProfilesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
